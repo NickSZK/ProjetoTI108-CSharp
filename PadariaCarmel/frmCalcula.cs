03e442afe4970407edacf59f52d648105647d711
NickSZK <sotrambike171@outlook.com>
2023-08-02 11:51:05 -0300

    Aula 02/08

--- PadariaCarmel/frmCalcula.cs
+++ PadariaCarmel/frmCalcula.cs
@@ -31,5 +31,54 @@ namespace PadariaCarmel {
 
             txtNum1.Focus();
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e) {
+            double num1, num2, resp = 0;
+
+            /*num1 = double.Parse(txtNum1.Text);
+            num2 = double.Parse(txtNum2.Text);*/
+
+            if (rdbSoma.Checked || rdbSubtracao.Checked || rdbMultiplicacao.Checked || rdbDivisao.Checked) {
+                try {
+                    num1 = Convert.ToDouble(txtNum1.Text);
+                    num2 = Convert.ToDouble(txtNum2.Text);
+
+                    if (rdbSoma.Checked) {
+                        resp = num1 + num2;
+                    } else if (rdbSubtracao.Checked) {
+                        resp = num1 - num2;
+                    } else if (rdbMultiplicacao.Checked) {
+                        resp = num1 * num2;
+                    } else {
+                        if (num2 == 0) {
+                            MessageBox.Show("Impossível por 0");
+
+                            txtNum1.Text = "";
+                            txtNum2.Text = "";
+                            rdbDivisao.Checked = false;
+
+                            txtNum1.Focus();
+
+                            lblResposta.Text = "";
+                        } else {
+                            resp = num1 / num2;
+                        }
+                    }
+
+                    lblResposta.Text = resp.ToString();
+                } catch (Exception) {
+                    MessageBox.Show("Favor inserie somene números");
+                    txtNum1.Text = "";
+                    txtNum2.Text = "";
+                    txtNum1.Focus();
+                }
+            } else {
+                MessageBox.Show("Por favor selecione uma operação", 
+                    "Mensagem do sistema", 
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning,
+                    MessageBoxDefaultButton.Button1);
+            }
+        }
     }
 }
